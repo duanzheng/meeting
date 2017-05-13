138ac8cbfa29102a23e770f463bf8a2c07f897c0
Tony <duanzheng21518@126.com>
2017-05-13 18:00:32 +0800

    1

--- page/component/index.js
+++ page/component/index.js
@@ -1,116 +1,116 @@
 Page({
-  data: {
-    dateList: [
-      {
-        id: 0,
-        text: '今天05-13',
-        select: true
-      },
-      {
-        id: 1,
-        text: '05-14',
-        select: false
-      },
-      {
-        id: 2,
-        text: '05-15',
-        select: false
-      },
-      {
-        id: 3,
-        text: '05-16',
-        select: false
-      },
-      {
-        id: 4,
-        text: '05-17',
-        select: false
-      },
-      {
-        id: 5,
-        text: '05-18',
-        select: false
-      },
-      {
-        id: 6,
-        text: '05-19',
-        select: false
-      },
-    ],
-    roomList: [],
-    timeList: [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-  },
-  selectDate: function (e) {
-    var selectId = e.currentTarget.id;
-    var oriDateList = this.data.dateList;
-    for (var i = 0; i < oriDateList.length; i++) {
-      if (oriDateList[i].id == selectId) {
-        oriDateList[i].select = true;
-      } else {
-        oriDateList[i].select = false;
-      }
-    }
-    this.setData({
-      dateList: oriDateList
-    });
-  },
-  //firstTime大于secondTime则返回true
-  transformTime: function (oriTime) {
-    var timeArray = oriTime.split(':');
-    return parseInt(timeArray[0]) + parseInt(timeArray[1]) / 60;
-  },
-  //minute为0代表0-30分，为1代表30-60分
-  judgeInterval: function (hour, minute, startTime, endTime) {
-    var ret = false;
-    var curStartTime = hour + ':' + (minute == 0 ? '00' : '30');
-    var curEndTIme = (minute == 0 ? hour : hour + 1) + ':' + (minute == 0 ? '30' : '00');
-    if (this.transformTime(curStartTime) >= this.transformTime(startTime) && this.transformTime(curEndTIme) <= this.transformTime(endTime)) {
-      ret = true;
-    }
-    return ret;
-  },
-  onReady: function () {
-    var that = this;
-    wx.request({
-      url: 'http://127.0.0.1:3000/roomList/05-13',
-      success: function (result) {
-        console.log(result);
-        var roomList = result.data;
-        for (var i = 0; i < roomList.length; i++) {
-          var roomRecord = roomList[i].record;
-          var timeList = [];
-          for (var j = 9; j <= 21; j++) {
-            var am = false;
-            var pm = false;
-
-            for (var k = 0; k < roomRecord.length; k++) {
-              am = am || that.judgeInterval(j, 0, roomRecord[k].startTime, roomRecord[k].endTime);
-              pm = pm || that.judgeInterval(j, 1, roomRecord[k].startTime, roomRecord[k].endTime);
+    data: {
+        dateList: [
+            {
+                id: 0,
+                text: '今天05-13',
+                select: true
+            },
+            {
+                id: 1,
+                text: '05-14',
+                select: false
+            },
+            {
+                id: 2,
+                text: '05-15',
+                select: false
+            },
+            {
+                id: 3,
+                text: '05-16',
+                select: false
+            },
+            {
+                id: 4,
+                text: '05-17',
+                select: false
+            },
+            {
+                id: 5,
+                text: '05-18',
+                select: false
+            },
+            {
+                id: 6,
+                text: '05-19',
+                select: false
+            },
+        ],
+        roomList: [],
+        curDate: '05-13'
+    },
+    selectDate: function (e) {
+        var selectId = e.currentTarget.id;
+        var oriDateList = this.data.dateList;
+        for (var i = 0; i < oriDateList.length; i++) {
+            if (oriDateList[i].id == selectId) {
+                oriDateList[i].select = true;
+            } else {
+                oriDateList[i].select = false;
             }
-            timeList.push({
-              value: j,
-              am: am,
-              pm: pm
-            });
-            roomList[i].timeList = timeList;
-          }
         }
-        console.log(roomList);
-        that.setData({
-          roomList: roomList
-        })
-      }
-    });
+        this.setData({
+            dateList: oriDateList
+        });
+    },
+    //firstTime大于secondTime则返回true
+    transformTime: function (oriTime) {
+        var timeArray = oriTime.split(':');
+        return parseInt(timeArray[0]) + parseInt(timeArray[1]) / 60;
+    },
+    //minute为0代表0-30分，为1代表30-60分
+    judgeInterval: function (hour, minute, startTime, endTime) {
+        var ret = false;
+        var curStartTime = hour + ':' + (minute == 0 ? '00' : '30');
+        var curEndTIme = (minute == 0 ? hour : hour + 1) + ':' + (minute == 0 ? '30' : '00');
+        if (this.transformTime(curStartTime) >= this.transformTime(startTime) && this.transformTime(curEndTIme) <= this.transformTime(endTime)) {
+            ret = true;
+        }
+        return ret;
+    },
+    onReady: function () {
+        var that = this;
+        wx.request({
+            url: 'http://127.0.0.1:3000/roomList/05-13',
+            success: function (result) {
+                console.log(result);
+                var roomList = result.data;
+                for (var i = 0; i < roomList.length; i++) {
+                    var roomRecord = roomList[i].record;
+                    var timeList = [];
+                    for (var j = 9; j <= 21; j++) {
+                        var am = false;
+                        var pm = false;
 
-    var timeList = [];
-    for (var i = 9; i <= 21; i++) {
-      timeList.push({
-        value: i
-      });
+                        for (var k = 0; k < roomRecord.length; k++) {
+                            am = am || that.judgeInterval(j, 0, roomRecord[k].startTime, roomRecord[k].endTime);
+                            pm = pm || that.judgeInterval(j, 1, roomRecord[k].startTime, roomRecord[k].endTime);
+                        }
+                        timeList.push({
+                            value: j,
+                            am: am,
+                            pm: pm
+                        });
+                        roomList[i].timeList = timeList;
+                    }
+                }
+                console.log(roomList);
+                that.setData({
+                    roomList: roomList
+                })
+            }
+        });
+
+        var timeList = [];
+        for (var i = 9; i <= 21; i++) {
+            timeList.push({
+                value: i
+            });
+        }
+        this.setData({
+            timeList: timeList
+        });
     }
-    this.setData({
-      timeList: timeList
-    });
-  }
 })
 
--- page/roomDetail/roomDetail.js
+++ page/roomDetail/roomDetail.js
@@ -1,27 +1,45 @@
 // roomDetail.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    roomId: '',
-    curDate: '2017-05-13'
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        roomId: '',
+        name: '',
+        desc: '',
+        curDate: '2017-05-13'
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(options);
-    this.setData({
-      roomId: options.roomId
-    })
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.setData({
+            roomId: options.roomId,
+            curDate: '2017-' + options.date
+        });
+        this.getRoomMsg();
+    },
 
-  bindDateChange: function (e) {
-    this.setData({
-      curDate: e.detail.value
-    })
-  }
+    bindDateChange: function (e) {
+        this.setData({
+            curDate: e.detail.value
+        })
+    },
+
+    getRoomMsg: function () {
+        var that = this;
+        wx.request({
+            url: 'http://127.0.0.1:3000/roomDetail?roomId=' + this.data.roomId + '&date=' + this.data.curDate,
+            success: function (result) {
+                const roomMsg = result.data;
+                console.log(roomMsg);
+                that.setData({
+                    name: roomMsg.name,
+                    desc: '可容纳：' + roomMsg.fill + '人，可用设备：' + roomMsg.equip
+                })
+            }
+        });
+    }
 })
